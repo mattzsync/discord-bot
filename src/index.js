719b2582fc3fa14633acbf77f2b1ddcf2e029fe7
mattzsync <matheus19brayan@gmail.com>
2025-09-03 08:43:53 -0400

    Inition commit with some code

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const { Client, IntentsBitField} = require('discord.js');
+require("dotenv").config(); // Load .env
+
+// Intents are a sets of permissions for actions
+// The variable client will act as the initializer
+const client = new Client({
+	intents: [
+		IntentsBitField.Flags.Guilds,
+		IntentsBitField.Flags.GuildMembers,
+		IntentsBitField.Flags.GuildMessages,
+		IntentsBitField.Flags.MessageContent,
+	],
+});
+
+// Use the token from .env
+client.login(process.env.DISCORD_BOT_TOKEN);
